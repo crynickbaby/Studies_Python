b784ed8cc7d0c7728a3ae9cfbe6b6795932eb2c7
Nicolas Santos <jarbascow@gmail.com>
2022-01-30 17:42:32 -0300

    Update Exercicios04.py

--- Exercicios/Exercicios04.py
+++ Exercicios/Exercicios04.py
@@ -1,5 +1,6 @@
 # Contagem de fogos
 
+from datetime import date
 from time import sleep
 for contagem in range(10, 0, -1):
     print(contagem)
@@ -40,3 +41,48 @@ if ftras == fcort:
     print('A frase é um palíndromo!')
 else:
     print('A frase não é um palíndromo')
+
+# Maiores de idade
+
+
+s = 0
+m = 0
+ano = date.today().year
+
+for c in range(1, 8):
+    nasc = int(input(f'Digite o ano que a {c}ª pessoa nasceu: '))
+    maioridade = ano - nasc
+    if maioridade >= 21:
+        s += 1
+    elif maioridade < 21:
+        m += 1
+print(f'Há \033[032m{s}\033[m pessoas que já são maior de idade e \033[033m{m}\033[m pessoas que não são maior de idade!')
+
+# Listagem de pessoas
+
+sidades = 0
+mvelho = 0
+cmulher = 0
+maisvelho = []
+nomemulher = []
+for i in range(1, 5):
+    nome = str(input(f'Nome da pessoa número {i}: '))
+    idade = int(input(f'Idade da pessoa número {i}: '))
+    sexo = int(input(f"""Sexo da pessoa número {i}: 
+    [ 4 ] Masculino
+    [ 5 ] Feminino
+    Sua opção: """))
+    sidades = sidades + idade
+    if i == 1 and sexo == 4:  # NOME DO MAIS VELHO
+        maisvelho = nome
+        mvelho = idade
+    else:
+        if idade > mvelho and sexo == 4:
+            maisvelho = nome
+            mvelho = idade
+    if sexo == 5 and idade < 20:
+        cmulher = cmulher + 1
+        nomemulher = nome
+print(f'A média de idades é: {sidades / 4}')
+print(f'O homem mais velho é o {maisvelho} com {mvelho} anos')
+print(f'{cmulher} mulher tem menos de 20 anos e é a {nomemulher}!')
